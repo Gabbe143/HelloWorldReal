1f27ebb8068c12f61a6888a10729bcd6d635be45
Gabriel <gabriel.jacobsen@outlook.com>
2022-01-15 03:29:20 +0100

    I only got option 6 and 7 left to do.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,28 +26,35 @@ fun main(args: Array<String>) {
                 People.add(Person(FirstName, LastName, Number, Email))
             }
             2 ->{
-                println("Please type in the individual's surname")
-                var TempFirstName: String = readln()
-                println("And please type in the individual's lastname")
-                var TempLastName: String = readln()
-                var TempIndex: Int = 0
-                for(Person in People){
-                    if(Person.FirstName.uppercase() == TempFirstName.uppercase() && Person.LastName.uppercase() == TempLastName.uppercase()){
-                        People.removeAt(TempIndex)
-                        println("The individual has successfully been removed from the contact list")
-                        break
-                    }
-                    TempIndex += 1
-                }
-                println("The person was unfortunately not found, perhaps you misspelled their surname and/or lastname")
-            }
-            3 ->{
                 var TempIndex: Int = 1
                 for(Person in People){
                     println("" + TempIndex + "." + " Firstname: "+ Person.FirstName + ", Lastname: " + Person.LastName + ", Number: " + Person.Number + ", Email: " + Person.Email)
                     TempIndex += 1
                 }
-                TempIndex = 0
+                println("Please type in the individual's surname")
+                var TempFirstName: String = readln()
+                println("And please type in the individual's lastname")
+                var TempLastName: String = readln()
+                var SizeOfPeopleList = 1
+                for(Person in People){
+                    if(Person.FirstName.uppercase() == TempFirstName.uppercase() && Person.LastName.uppercase() == TempLastName.uppercase()){
+                        People.removeAt(SizeOfPeopleList-1)
+                        println("The individual has successfully been removed from the contact list")
+                        break
+                    }
+                    if (SizeOfPeopleList == People.size) {
+                        println("The person was unfortunately not found, perhaps you misspelled their surname and/or lastname")
+                        break
+                    }
+                    SizeOfPeopleList += 1
+                }
+            }
+            3 ->{
+                var TempIndex: Int = 1
+                for(Person in People) {
+                    println("" + TempIndex + "." + " Firstname: " + Person.FirstName + ", Lastname: " + Person.LastName + ", Number: " + Person.Number + ", Email: " + Person.Email)
+                    TempIndex += 1
+                }
                 println("What is the first and last name of the contact that you would like to edit?")
                 var FirstnameAndLastname = readln()
                 for(Person in People){
@@ -83,7 +90,6 @@ fun main(args: Array<String>) {
                             }
                         }while(ContinueInLoop)
                     }
-                    TempIndex += 1
                 }
             }
             4 ->{
@@ -94,6 +100,11 @@ fun main(args: Array<String>) {
                 }
             }
             5 ->{
+                var TempIndex: Int = 1
+                val SortedList = People.sortedBy { it.FirstName }
+                for(Person in SortedList){
+                    println("" + TempIndex + "." + " Firstname: " + Person.FirstName + ", Lastname: " + Person.LastName + ", Number: " + Person.Number + ", Email: " + Person.Email)
+                }
 
             }
             6 ->{
@@ -109,7 +120,7 @@ fun main(args: Array<String>) {
                 println("You have typed a invalid answer, try again.")
             }
         }
-    }while(StayInLoop == true)
+    }while(StayInLoop)
     println("You have successfully turned off the application, all of the contacts will be automatically saved.")
 
 
@@ -120,5 +131,4 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
-
 class Person(var FirstName: String, var LastName: String, var Number: String, var Email: String)
